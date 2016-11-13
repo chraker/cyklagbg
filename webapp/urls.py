620bef27aa393ae7ff23f1b5ec948602d012ca5b
Christopher Åkersten <Christopher kersten>
2016-11-13 14:59:16 +0100

    fixed navigations and views

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from webapp import views
 
 urlpatterns = [
-    url(r'^$', views.home, name = "home"),
-    url(r'^bikes/$', views.getbike, name='getbike'),
+    url(r'^$', views.findbike, name = "findbike"),
+    url(r'^directions/$', views.directions, name='directions'),
+    url(r'^stations/$', views.stations, name='stations'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -10,41 +10,44 @@ from django.template.response import TemplateResponse
 from webapp.models import BikeStation
 
 
-def home(request):
+def findbike(request):
     data = BikeStation.objects.all()
-    return TemplateResponse(request, 'index.html', {'data' : data})
+    return TemplateResponse(request, 'findbike.html', {'data' : data, 'nbar': ''})
+
+def stations(request):
+    data = BikeStation.objects.all()
+    return TemplateResponse(request, 'stations.html', {'data' : data, 'nbar': 'stations'})
 
 def directions(request):
-    context = RequestContext(request)
-    if request.method == 'GET':
-        location = request.GET['location']
+    data = BikeStation.objects.all()
+    return TemplateResponse(request, 'directions.html', {'data' : data, 'nbar': 'directions'})
 
-def getbike(request):
-    if request.method == 'GET':
-        # get location as long/lat
-        request_str = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCGTxlg3YzIgRVdPD3HFpYlMkrSyxQNHyI"
-        request_str += "&address=" + request.GET['location'].replace(", ","+")
-
-        location = requests.get(request_str).content.decode("utf-8").replace("\n","")
-        location = json.loads(location)
-        loc_data = location["results"][0]["geometry"]["location"]
-        locatior_str = str(loc_data["lat"])+","+ str(loc_data["lng"])
-
-        # make matrix search for bike stations
-        request_str = "https://maps.googleapis.com/maps/api/distancematrix/json?key=AIzaSyCGTxlg3YzIgRVdPD3HFpYlMkrSyxQNHyI"
-        request_str += "&origins=" + locatior_str
-        request_str +="&destiantions="
-        data = BikeStation.objects.all()
-        for station in data:
-            if station.available_bikes > 0 :
-                request_str += str(station.lat)
-                request_str += ","
-                request_str += str(station.long)
-                request_str += "|"
-
-        request_str += "&mode=walking"
-
-        r = requests.get(request_str)
-
-        # get closest station
-        r.doh
+# def getbike(request):
+#     if request.method == 'GET':
+#         # get location as long/lat
+#         request_str = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCGTxlg3YzIgRVdPD3HFpYlMkrSyxQNHyI"
+#         request_str += "&address=" + request.GET['location'].replace(", ","+")
+#
+#         location = requests.get(request_str).content.decode("utf-8").replace("\n","")
+#         location = json.loads(location)
+#         loc_data = location["results"][0]["geometry"]["location"]
+#         locatior_str = str(loc_data["lat"])+","+ str(loc_data["lng"])
+#
+#         # make matrix search for bike stations
+#         request_str = "https://maps.googleapis.com/maps/api/distancematrix/json?key=AIzaSyCGTxlg3YzIgRVdPD3HFpYlMkrSyxQNHyI"
+#         request_str += "&origins=" + locatior_str
+#         request_str +="&destiantions="
+#         data = BikeStation.objects.all()
+#         for station in data:
+#             if station.available_bikes > 0 :
+#                 request_str += str(station.lat)
+#                 request_str += ","
+#                 request_str += str(station.long)
+#                 request_str += "|"
+#
+#         request_str += "&mode=walking"
+#
+#         r = requests.get(request_str)
+#
+#         # get closest station
+#         r.doh
